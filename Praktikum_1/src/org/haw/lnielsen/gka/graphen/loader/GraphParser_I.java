8a0c70764277317c850896efaefd4405f00d6f1d
Lars 'Xancake' Nielsen <xanylein@web.de>
2015-03-31 20:42:02 +0200

    (vorläufige) Interfaces für den Parser und das UserInterface angelegt

--- Praktikum_1/src/org/haw/lnielsen/gka/graphen/loader/GraphParser_I.java
+++ Praktikum_1/src/org/haw/lnielsen/gka/graphen/loader/GraphParser_I.java
@@ -7,5 +7,11 @@ import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 
 public interface GraphParser_I {
+	/**
+	 * Liest einen Graphen aus einem {@link InputStream}.
+	 * @param in Der Inputstream
+	 * @return Der gelesene Graph
+	 * @throws IOException Wenn ein Fehler beim Zugriff auf den Inputstream auftritt
+	 */
 	Graph<Knoten, DefaultEdge> parseGraph(InputStream in) throws IOException;
 }
---
+++ Praktikum_1/src/org/haw/lnielsen/gka/graphen/ui/SimpleGraphUI_I.java
@@ -0,0 +1,13 @@
+package org.haw.lnielsen.gka.graphen.ui;
+
+import org.haw.lnielsen.gka.graphen.Knoten;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+
+public interface SimpleGraphUI_I {
+	/**
+	 * Zeigt den übergebenen Graphen an.
+	 * @param graph Der anzuzeigende Graph
+	 */
+	void showGraph(Graph<Knoten, DefaultEdge> graph);
+}
