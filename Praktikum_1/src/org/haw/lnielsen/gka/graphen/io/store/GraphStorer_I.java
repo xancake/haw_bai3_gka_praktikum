865244025e0c3635780fce4a57bf103b9279d692
Lars 'Xancake' Nielsen <xanylein@web.de>
2015-04-29 13:22:30 +0200

    Javadoc für den GraphStorer

--- Praktikum_1/src/org/haw/lnielsen/gka/graphen/io/store/GraphStorer_I.java
+++ Praktikum_1/src/org/haw/lnielsen/gka/graphen/io/store/GraphStorer_I.java
@@ -7,6 +7,11 @@ import org.haw.lnielsen.gka.graphen.Knoten;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 
+/**
+ * Die Schnittstelle für einen Graph-Schreiber.
+ * 
+ * @author Lars Nielsen
+ */
 public interface GraphStorer_I {
 	/**
 	 * Speichert den übergebenen Graphen.
