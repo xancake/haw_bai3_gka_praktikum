b1ccec6392a91769981cd31164dcbd16a546daad
xancake <xanylein@web.de>
2015-06-12 14:23:04 +0200

    Tests für die Euleralgorithmen angelegt

---
+++ Praktikum_1/test/org/haw/lnielsen/gka/graphen/algorithm/euler/EulerAlgorithmTest_A.java
@@ -0,0 +1,70 @@
+package org.haw.lnielsen.gka.graphen.algorithm.euler;
+
+import static org.haw.lnielsen.gka.graphen.algorithm.euler.EulerAsserts.*;
+import static org.junit.Assert.*;
+
+import org.haw.lnielsen.gka.graphen.Knoten;
+import org.haw.lnielsen.gka.graphen.io.loader.GKAGraphParser;
+import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
+import org.jgrapht.graph.DefaultEdge;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Allgemeine Testfälle für Euler-Algorithmen.
+ * 
+ * @author Lars Nielsen
+ * @author Jennifer Momsen
+ */
+public abstract class EulerAlgorithmTest_A {
+	private EulerAlgorithm_I<Knoten, DefaultEdge> myAlgorithm;
+	
+	@Before
+	public void setUp() {
+		myAlgorithm = createEulerAlgorithm();
+	}
+	
+	@Test
+	public void testFindEulerTour_Eulerpfad_Nikolaushaus() throws Exception {
+		Graph<Knoten, DefaultEdge> graph = new GKAGraphParser().parseGraph(ClassLoader.getSystemResourceAsStream("loader/euler/eulerpfad_nikolaushaus.graph"));
+		GraphPath<Knoten, DefaultEdge> eulerpath = myAlgorithm.findEulerTour(graph);
+		assertNull(eulerpath);
+		// TODO: Vielleicht eine findEulerPath-Methode implementieren?
+	}
+	
+	@Test
+	public void testFindEulerTour_Eulertour_1() throws Exception {
+		Graph<Knoten, DefaultEdge> graph = new GKAGraphParser().parseGraph(ClassLoader.getSystemResourceAsStream("loader/euler/eulertour_1.graph"));
+		GraphPath<Knoten, DefaultEdge> eulerpath = myAlgorithm.findEulerTour(graph);
+		assertEulerTour(eulerpath);
+	}
+	
+	@Test
+	public void testFindEulerTour_Eulertour_2() throws Exception {
+		Graph<Knoten, DefaultEdge> graph = new GKAGraphParser().parseGraph(ClassLoader.getSystemResourceAsStream("loader/euler/eulertour_2.graph"));
+		GraphPath<Knoten, DefaultEdge> eulerpath = myAlgorithm.findEulerTour(graph);
+		assertEulerTour(eulerpath);
+	}
+	
+	@Test
+	public void testFindEulerTour_NoEulertour_1() throws Exception {
+		Graph<Knoten, DefaultEdge> graph = new GKAGraphParser().parseGraph(ClassLoader.getSystemResourceAsStream("loader/euler/noeulertour_1.graph"));
+		GraphPath<Knoten, DefaultEdge> eulerpath = myAlgorithm.findEulerTour(graph);
+		assertEulerTour(eulerpath);
+	}
+	
+	@Test
+	public void testFindEulerTour_NoEulertour_2() throws Exception {
+		Graph<Knoten, DefaultEdge> graph = new GKAGraphParser().parseGraph(ClassLoader.getSystemResourceAsStream("loader/euler/noeulertour_2.graph"));
+		GraphPath<Knoten, DefaultEdge> eulerpath = myAlgorithm.findEulerTour(graph);
+		assertEulerTour(eulerpath);
+	}
+	
+	/**
+	 * Fabrikmethode für erbende Tests um ihre entsprechende Implementierung des Algorithmus
+	 * bereitzustellen.
+	 * @return Der Algorithmus
+	 */
+	protected abstract EulerAlgorithm_I<Knoten, DefaultEdge> createEulerAlgorithm();
+}
---
+++ Praktikum_1/test/org/haw/lnielsen/gka/graphen/algorithm/euler/EulerAsserts.java
@@ -0,0 +1,48 @@
+package org.haw.lnielsen.gka.graphen.algorithm.euler;
+
+import static org.junit.Assert.*;
+import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
+
+/**
+ * Util-Klasse für asserts bezüglich der Euler-Algorithmen.
+ *  
+ * @author Lars Nielsen
+ * @author Jennifer Momsen
+ */
+public class EulerAsserts {
+	private EulerAsserts() {}
+	
+	/**
+	 * Prüft, ob der übergebene {@link GraphPath} eine Euler-Tour repräsentiert.
+	 * @param path Der zu testende {@link GraphPath}
+	 */
+	public static <V, E> void assertEulerTour(GraphPath<V, E> path) {
+		assertEquals(path.getStartVertex(), path.getEndVertex());
+		assertEulerPath(path);
+	}
+	
+	/**
+	 * Prüft, ob der übergebene {@link GraphPath} einen Euler-Pfad repräsentiert.
+	 * @param path Der zu testende {@link GraphPath}
+	 */
+	public static <V, E> void assertEulerPath(GraphPath<V, E> path) {
+		Graph<V, E> graph = path.getGraph();
+		for(E edge : graph.edgeSet()) {
+			assert(path.getEdgeList().contains(edge));
+		}
+		
+		V previous = path.getStartVertex();
+		for(E edge : path.getEdgeList()) {
+			V source = graph.getEdgeSource(edge);
+			V target = graph.getEdgeTarget(edge);
+			if(previous.equals(source)) {
+				previous = target;
+			} else if(previous.equals(target)) {
+				previous = source;
+			} else {
+				fail("Vorgängerknoten ist nicht in der aktuell betrachteten Kante enthalten!");
+			}
+		}
+	}
+}
